91ab0c77e3c3da61647bb56cf8facea0f680553d
Marcos <mp2772@gmail.com>
2020-01-27 18:52:47 -0500

    updated comments

--- routes/create-read/form-page.js
+++ routes/create-read/form-page.js
@@ -4,48 +4,56 @@ const Task = require("../../models/Task");
 
 // GET Form Page
 router.get("/form", (req, res, next) => {
-  res.render("form-views/form");
+    res.render("form-views/form");
 });
 
 // Post Form Input
+// the endpoint that you create here is what you put in the action field of a form that you want to call this route and retrieve the values of the inputs.
 router.post("/form-input", (req, res, next) => {
-  console.log("the form info: ", req.body);
+    // req.body is used to grab the value of an input which would normally be a post request.
+    // in order to have a value in req.body, you must have a name field for each input that you wish to get the value from.
+    console.log("the form info: ", req.body);
 
-  Task.create(req.body)
-    .then(taskFromDB => {
-      console.log("the newly created task: ", { taskFromDB });
+    Task.create(req.body)
+        .then(taskFromDB => {
+            console.log("the newly created task: ", { taskFromDB });
 
-      res.redirect(`/task-details/${taskFromDB._id}`);
-    })
-    .catch(err => next(err));
+            res.redirect(`/task-details/${taskFromDB._id}`);
+        })
+        .catch(err => next(err));
 });
 
 // View Task List
 router.get("/task-list", (req, res, next) => {
-  Task.find()
-    .then(tasksFromDB => {
-      const data = {
-        pageTitle: "Task List",
-        tasks: tasksFromDB
-      };
+    Task.find()
+        .then(tasksFromDB => {
+            const data = {
+                pageTitle: "Task List",
+                tasks: tasksFromDB
+            };
 
-      res.render("task-views/task-list", data);
-    })
-    .catch(err => next(err));
+            res.render("task-views/task-list", data);
+        })
+        .catch(err => next(err));
 });
 
 // View Details of task
 router.get("/task-details/:taskId", (req, res, next) => {
-  Task.findById(req.params.taskId)
-    .then(taskFromDB => {
-      const data = {
-        pageTitle: taskFromDB.title + " Details",
-        task: taskFromDB
-      };
+    // req.params is a variable that you set for your end point (:taskId).
+    // you would have to put a : in-front of the variable that you are creating
+    // and the value of that variable will be whatever is passed in the url endpoint when calling the route.
 
-      res.render("task-views/task-details", data);
-    })
-    .catch(err => next(err));
+    // the req.param.taskId is used here to search the database for the specific task that we are using for by passing the tasks unique _id that we get when it is created on the database.
+    Task.findById(req.params.taskId)
+        .then(taskFromDB => {
+            const data = {
+                pageTitle: taskFromDB.title + " Details",
+                task: taskFromDB
+            };
+
+            res.render("task-views/task-details", data);
+        })
+        .catch(err => next(err));
 });
 
 module.exports = router;
